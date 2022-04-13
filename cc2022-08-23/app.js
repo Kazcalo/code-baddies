cf138a898c63f5368b3128c1dcefbbab2b49d99b
kazcalo <storm.west@icloud.com>
2022-04-13 16:22:29 -0400

    first try

---
+++ cc2022-08-23/app.js
@@ -0,0 +1,18 @@
+//problem:Make a function that will return a greeting statement that uses an input; your program should return,
+//Example: "Hello, <name> how are you doing today?".
+
+
+
+
+ 
+//My answer: Right
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+  }
+
+  //Code war answer
+  const greet = name => `Hello, ${name} how are you doing today?`;
+//Code war answer
+function greet(name) {
+    return "Hello, " + name + " how are you doing today?";
+  }
