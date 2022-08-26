d592a1586024844959b708a5cbd62b4e1c97eabe
kazcalo <storm.west@icloud.com>
2022-08-25 23:38:29 -0400

    1

---
+++ cc2022-08-25/app.js
@@ -0,0 +1,15 @@
+//problem:Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+//Example: (Input1, Input2 -->Output)
+
+
+
+ 
+//My answer: Right
+const sumStr = (a, b) => String(+a + +b)
+
+  //Code war answer
+  
+//Code war answer
+ 
+ 
